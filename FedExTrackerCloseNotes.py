7ef8e7301b8b971d6cd7d6e3c164b12487300b4d
Isaac Cloud <isaacmcloud@gmail.com>
2025-04-04 09:17:42 -0400

    imported the PyperClip module to allow for the output to be copied to the clipboard. Fixed some quotations that was causing the link generation not to work. Changed some of the output wording. Added emojis to the script output in terminal, not what is copied to the clipboard.

--- FedExTrackerCloseNotes.py
+++ FedExTrackerCloseNotes.py
@@ -1,9 +1,20 @@
-                                                     
+# using pyperclip module to copy the final text output to the clipboard (windows) / https://pypi.org/project/pyperclip/
+import pyperclip
+
+
 tracking_number = input("Enter FedEx Tracking Number: ")
 user_first_name = input("Enter user's First Name: ")
 
+# Link generated is wrapped with the [code] tags to format the html hyperlink in Service Now links
 tracking_link = f'[code]<a href="https://www.fedex.com/fedextrack/?trknbr={tracking_number}">click here</a>[/code]'
 
-print(tracking_link)
+rma_notes = f"""Hello {user_first_name},
 
-print(f"Hello {user_first_name},\n\nThank you for your order. Please {tracking_link} and navigate to the FedEx website to track your RMA request; note that it may take up to 24 hours for your package to appear in the FedEx system.\n\nThank you,")
+Thank you for your order. Please {tracking_link} and navigate to the FedEx website to track your RMA request. Note that it may take up to 24 hours for your package to appear in the FedEx system.
+
+Thank you,
+"""
+
+print(rma_notes)
+pyperclip.copy(rma_notes)
+print("✅ RMA notes copied to clipboard! 📋")

bbc4c240da3c9b06436d08150c7f352fafc9ec14
Isaac Cloud <isaacmcloud@gmail.com>
2025-04-04 08:54:26 -0400

    added the [code] tags for Service Now to create a hyperlink out of the inputed tracking number which then formats to a trackink link

--- FedExTrackerCloseNotes.py
+++ FedExTrackerCloseNotes.py
@@ -1,8 +1,9 @@
                                                      
 tracking_number = input("Enter FedEx Tracking Number: ")
 user_first_name = input("Enter user's First Name: ")
-tracking_link = "https://www.fedex.com/fedextrack/?trknbr=" + tracking_number
+
+tracking_link = f'[code]<a href="https://www.fedex.com/fedextrack/?trknbr={tracking_number}">click here</a>[/code]'
 
 print(tracking_link)
 
-print(f"Hello {user_first_name},\n\nThank you for your order. Please click here and navigate to the FedEx website to track your RMA request; note that it may take up to 24 hours for your package to appear in the FedEx system.\n\nThank you,")
+print(f"Hello {user_first_name},\n\nThank you for your order. Please {tracking_link} and navigate to the FedEx website to track your RMA request; note that it may take up to 24 hours for your package to appear in the FedEx system.\n\nThank you,")
